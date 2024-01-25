0d2921e66e13446f047e51470dce4bcb35f75020
bulat <farukshin@molot.ru>
2024-01-25 15:30:40 +0300

    *  v-for

--- myvue/src/App.vue
+++ myvue/src/App.vue
@@ -1,19 +1,38 @@
 <script>
-
 export default {
   data() {
     return {
-
+      posts: [
+        { id: 1, title: 'Заголовок 1', body: 'Post body' },
+        { id: 2, title: 'Заголовок 2', body: 'Post body' },
+        { id: 3, title: 'Заголовок 3', body: 'Post body' },
+        { id: 4, title: 'Заголовок 4', body: 'Post body' }
+      ]
     }
   },
+  methods: {}
 }
-
 </script>
 
 <template>
   <div>
-    test
+    <div class="post" v-for="post in posts">
+      <div><strong>Название</strong> {{ post.title }}</div>
+      <div><strong>Описание</strong> {{ post.body }}</div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin: 5px;
+}
+</style>
